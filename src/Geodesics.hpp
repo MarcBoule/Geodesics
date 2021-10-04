4573a16511ff559f0b8f2005835a4212f24aed56
MarcBoule <marc.boule@etsmtl.ca>
2021-10-04 10:24:35 -0400

    tweak led bg color to make led colors less washed out

--- src/Geodesics.hpp
+++ src/Geodesics.hpp
@@ -134,8 +134,17 @@ struct BlankCKnob : DynamicSVGKnob {
 
 struct GeoGrayModuleLight : ModuleLightWidget {
 	GeoGrayModuleLight() {
-		bgColor = nvgRGB(0x8e, 0x8e, 0x8e);
-		borderColor = nvgRGB(0x1d, 0x1d, 0x1b);//nvgRGBA(0, 0, 0, 0x60);
+		// rack v1
+		// bgColor = nvgRGB(0x5a, 0x5a, 0x5a);
+		// borderColor = nvgRGBA(0, 0, 0, 0x60);
+
+		// rack v2
+		bgColor = nvgRGBA(0x33, 0x33, 0x33, 0xff);
+		borderColor = nvgRGBA(0, 0, 0, 53);
+		
+		// geodesics v1
+		// bgColor = nvgRGB(0x8e, 0x8e, 0x8e);
+		// borderColor = nvgRGB(0x1d, 0x1d, 0x1b);//nvgRGBA(0, 0, 0, 0x60);
 	}
 	
 	void drawLight(const DrawArgs &args) override { // from app/LightWidget.cpp (only nvgStrokeWidth of border was changed)
